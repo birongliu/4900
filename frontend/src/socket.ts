2996240fc1aff8b81ba039fff39a07c6118c32b8
birongliu <birongliu2003@gmail.com>
2024-12-08 16:59:10 -0500

    Update socket connection to use environment variable for WebSocket URL

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -2,4 +2,4 @@
 
 import { io } from "socket.io-client";
 
-export const socket = io(`ws://${process.env.NEXT_PUBLIC_API_URL}`, { withCredentials: true });;
+export const socket = io(`ws://${process.env.NEXT_PUBLIC_WS_URL}`, { withCredentials: true });;
